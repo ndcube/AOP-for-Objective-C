bf255415fb26c4bd34f90c292d1539f0b5bc9aa8
Marcelo Emmerich <marcelo.emmerich@gmail.com>
2012-10-08 15:53:00 +0200

    initial commit

---
+++ AOPAspect/AOPAspect/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AOPAspect
+//
+//  Created by Marcelo Emmerich on 08.10.12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, @"GHUnitIOSAppDelegate");
+    }
+}
